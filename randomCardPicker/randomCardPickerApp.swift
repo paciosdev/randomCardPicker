b4764ec6395326fec94791a0bb15e7ede39680a0
Francesco Paciello <kekkopacio@gmail.com>
2022-01-25 19:57:10 +0100

    Initial Commit

---
+++ randomCardPicker/randomCardPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomCardPickerApp.swift
+//  randomCardPicker
+//
+//  Created by Kekko Paciello on 25/01/22.
+//
+
+import SwiftUI
+
+@main
+struct randomCardPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
